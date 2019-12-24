25d8875136cb1d4ae3ce75b45a757278143812b3
Martin Kapal <flamecze@gmail.com>
2019-12-24 13:41:10 +0100

    Code style

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import * as React from 'react';
 import { Provider } from 'react-redux';
+
 import { Dashboard } from './components/Dashboard/Dashboard';
 import store from './configureStore';
 
 import './App.css';
 
-const App = () => {
-  return (
-    <Provider store={store}>
-      <Dashboard />
-    </Provider>
-  );
-};
+const App: React.FC = () => (
+  <Provider store={store}>
+    <Dashboard />
+  </Provider>
+);
 
 export default App;
